9c4f877eecd746ab643ee666b2a92f3f76e453e4
chhunhak16 <rchhunhak@gmail.com>
2025-07-20 12:21:37 +0700

    fetch fix

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://tmhvojlsojlgysdfeeqd.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRtaHZvamxzb2psZ3lzZGZlZXFkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTI5ODM0NDcsImV4cCI6MjA2ODU1OTQ0N30.TxD4I0AX0PX3k7ZhPIMmqtI6R7HAJ98ca7pWqOQAhko';
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
